ecb349ac4184d85388a810237c7181763f271ad8
sahal-bs23 <sahal@brainstation-23.com>
2025-05-06 15:28:05 +0600

    commit - 24

---
+++ consumer/src/main/java/com/wallet/consumer/dto/Customer.java
@@ -0,0 +1,12 @@
+package com.wallet.consumer.dto;
+
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+}
