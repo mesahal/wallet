a5abae586ca85f95735c9363fdcd5cfe38efc3ff
sahal-bs23 <sahal@brainstation-23.com>
2025-05-19 14:41:48 +0600

    Added exceptions

---
+++ charge-and-fee-service/src/main/java/com/wallet/charge_and_fee_service/ChargeAndFeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wallet.charge_and_fee_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeAndFeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargeAndFeeServiceApplication.class, args);
+	}
+
+}
---
+++ charge-and-fee-service/src/test/java/com/wallet/charge_and_fee_service/ChargeAndFeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallet.charge_and_fee_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeAndFeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
